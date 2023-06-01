da91c1a7b2e327dfc948598b5925a92dc775e767
Matheus <matheus.ramos.703@ufrn.edu.br>
2023-06-01 10:37:56 -0300

    novas modificações

--- app/app/tests.py
+++ app/app/tests.py
@@ -12,4 +12,9 @@ class CalcTests(SimpleTestCase):
     def test_add_numbers(self):
         res = calc.add(5, 6)
         self.assertEqual(res, 11) # irá testa se o resultado (primeiro parametro) condiz com o valor dado no segundo parametro
-        
+
+
+        def test_subtract_numbers(self):
+            res = calc.subtract(10, 15)
+
+            self.assertEqual(res, 5)
