cc868f92f19ffcba923d8ed73ae1de33a13b1ff8
sara.viegas <sara.viegas@pop-rn.rnp.br>
2023-07-18 11:32:55 -0300

    Aula 71

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -9,8 +9,8 @@ from django.test import Client
 
 class AdminSiteTests(TestCase):
     """Test for Django admin"""
-    
-    def setUp(self): 
+
+    def setUp(self):
         """Create user and client."""
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
@@ -27,7 +27,7 @@ class AdminSiteTests(TestCase):
     def test_users_lists(self):
         """Test that users are listed on page."""
         url = reverse('admin:core_user_changelist')
-        res = self.cleint.get(url)
+        res = self.client.get(url)
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
